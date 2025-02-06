270b5138f7f9ebae6b8747925f761e21e12fd103
dayofaniyan-liberis <109070982+dayofaniyan-liberis@users.noreply.github.com>
2025-02-06 01:14:41 +0000

    update RecipeRequestValidator

--- DepuChef.Api/Validators/RecipeRequestValidator.cs
+++ DepuChef.Api/Validators/RecipeRequestValidator.cs
@@ -1,17 +1,20 @@
 using DepuChef.Application.Models;
 using FluentValidation;
 
-public class RecipeRequestValidator : AbstractValidator<RecipeRequest>
+namespace DepuChef.Api.Validators
 {
-    public RecipeRequestValidator()
+    public class RecipeRequestValidator : AbstractValidator<RecipeRequest>
     {
-        RuleFor(x => x.Image)
-            .NotEmpty()
-            .Must(x => x!.Length <= 5_000_000)
-            .WithMessage("Image is too large.")
-            .Must(x => x!.ContentType.Contains("image"))
-            .WithMessage("File is not an image.");
-        RuleFor(x => x.ConnectionId)
-            .NotEmpty();
+        public RecipeRequestValidator()
+        {
+            RuleFor(x => x.Image)
+                .NotEmpty()
+                .Must(x => x!.Length <= 5_000_000)
+                .WithMessage("Image is too large.")
+                .Must(x => x!.ContentType.Contains("image"))
+                .WithMessage("File is not an image.");
+            RuleFor(x => x.ConnectionId)
+                .NotEmpty();
+        }
     }
 }
